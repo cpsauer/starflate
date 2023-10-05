c6c647a0c0639f6d432c6cb1908304c6a35cfbc1
Oliver Lee <oliverzlee@gmail.com>
2023-10-04 21:09:30 -0700

    always use DEFLATE canonical form for huffman::table codes
    Update huffman::table constructors to always call canonicalize(). As
    canonicalize() is now called in the constructor, this method is now
    private.
    
    Note that the table_contents constructor is an exception and does not
    call canonicalize().
    
    Change-Id: I5597dc51ea5e9f086c71e2f9396fccd5ace923b2

--- huffman/test/decode_test.cpp
+++ huffman/test/decode_test.cpp
@@ -16,34 +16,36 @@ auto main() -> int
   using namespace huffman::literals;
 
   test("basic") = [] {
-    // encoded data from dahuffman readme.rst, but in hex.
-    constexpr std::array encoded_bytes = {
-        std::byte{0x86},
-        std::byte{0x7c},
-        std::byte{0x25},
-        std::byte{0x13},
-        std::byte{0x69},
-        std::byte{0x40}};
+    constexpr auto encoded_bytes = std::array{
+        std::byte{0b0111'1101},
+        std::byte{0b1000'0011},
+        std::byte{0b1111'1010},
+        std::byte{0b1110'1100},
+        std::byte{0b1001'0110},
+        std::byte{0b1101'1110}};
 
-    constexpr char eot = {'\4'};
+    constexpr auto eot = char{'\4'};
     static constexpr auto code_table =  // clang-format off
       huffman::table{
         huffman::table_contents,
-        {std::pair{1_c,     'e'},
-                  {01_c,    'i'},
-                  {001_c,   'n'},
-                  {0001_c,  'q'},
-                  {00001_c, 'x'},
-                  {00000_c, eot}}
+        {std::pair{0_c,     'e'},
+                  {10_c,    'i'},
+                  {110_c,   'n'},
+                  {1110_c,  'q'},
+                  {11110_c, eot},
+                  {11111_c, 'x'}
+        }
       };  // clang-format on
 
-    constexpr std::array expected = {
+    constexpr auto expected = std::array{
         'e', 'x', 'e', 'n', 'e', 'e', 'e', 'e', 'x', 'n',
-        'i', 'q', 'n', 'e', 'i', 'e', 'i', 'n', 'i', eot,
+        'i', 'q', 'n', 'e', 'i', 'e', 'i', 'n', 'n', eot,
     };
     constexpr auto output_buf = [&] {
-      std::array<char, expected.size()> output_buf{};
-      auto result = decode(code_table, encoded_bytes, output_buf.begin());
+      auto output_buf = std::array<char, expected.size()>{};
+
+      const auto result = decode(code_table, encoded_bytes, output_buf.begin());
+
       // result should point to the back of output_buf.
       if (output_buf.end() != result) {
         throw std::runtime_error("assertion failed");
